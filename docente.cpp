06cbb6e0ceda8a5e4883ca5de4fd97314c613e69
Kaio Rosa <kaiosrosa@gmail.com>
2019-08-21 22:57:37 -0300

    add Docente Publicacao Veiculo

--- docente.cpp
+++ docente.cpp
@@ -1,5 +1,16 @@
+#include <iostream>
+#include <ctime>
+
 class Docente {
+    long codigo;
+    std::string nome;
+    time_t data_ingresso; // what is the best type to store date
+    bool isCoordenador;
     
     public:
+    // getters and setters
+    long get_codigo(){
+        return codigo;
+    }
 
 };
---
+++ veiculo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class Veiculos{
+    std::string sigla;
+    std::string nome;
+    char tipo;
+    double fatorDeImpacto;
+    std::string issn;
+    public:
+};
