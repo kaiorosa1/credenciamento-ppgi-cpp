f0c51a99592929cbf03e4b0df3badecc8b34971c
Kaio Rosa <kaiosrosa@gmail.com>
2019-08-24 20:37:33 -0300

    add input files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <vector>
+#include "docente.h"
+
+void readDocentesFile();
+void readVeiculosFile();
+void readPublicacoesFile();
+void readQualificacoesFile();
+void readRegrasPontuacaoFile();
 
 int main(){
-    std::cout<< "hello project" << std::endl;
+    Docente docente;
+    // read files
+    readDocentesFile();
+    readVeiculosFile();
+    readPublicacoesFile();
+    readQualificacoesFile();
+    readRegrasPontuacaoFile();
+
     return 0;
+}
+
+void readDocentesFile(){
+    std::cout<< "Read Docente.csv file!"<< std::endl;
+}
+
+void readVeiculosFile(){
+    std::cout<< "Read Veiculos.csv file!"<< std::endl;
+}
+void readPublicacoesFile(){
+    std::cout<< "Read Publicacoes.csv file!"<< std::endl;
+}
+void readQualificacoesFile(){
+    std::cout<< "Read Qualificacoes.csv file!"<< std::endl;
+}
+void readRegrasPontuacaoFile(){
+    std::cout<< "Read Regras.csv file!"<< std::endl;
 }
