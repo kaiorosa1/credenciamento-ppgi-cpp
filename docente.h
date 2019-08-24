36fa768e87edb803181608281e77efc8ab9d2c77
Kaio Rosa <kaiosrosa@gmail.com>
2019-08-23 22:27:18 -0300

    add docente getters and setters

--- docente.h
+++ docente.h
@@ -7,11 +7,19 @@ class Docente {
     long codigo;
     std::string nome;
     time_t data_ingresso; // what is the best type to store date
-    bool isCoordenador;
+    bool is_coordenador;
     
     public:
     // getters and setters
     long get_codigo(){ return codigo;}
+    std::string get_nome(){ return nome;}
+    time_t get_data_ingresso(){return data_ingresso;}
+    bool get_is_coodernador(){return is_coordenador;}
+
+    void set_codigo(long cod){ codigo = cod;}
+    void set_nome(std::string name){nome = name;}
+    void set_data_ingresso(time_t dt_ingresso){data_ingresso = dt_ingresso;}
+    void set_is_coordenador(bool is_coord){is_coordenador = is_coord;}
 
 };
 
