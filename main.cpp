3b8743cb8249dbf3b13152a5dfc8e5a18be10e1a
Kaio Rosa <kaiosrosa@gmail.com>
2019-08-25 18:56:05 -0300

    start reading first file

--- main.cpp
+++ main.cpp
@@ -10,20 +10,27 @@ void readQualificacoesFile();
 void readRegrasPontuacaoFile();
 
 int main(){
-
-    std::fstream fin;
     // open docentes file
-    fin.open("docentes.csv", std::ios::in);
-    
+    std::fstream file ("docentes.csv");
+    std::string input;
+    if(!file.is_open()){
+        std::cout << "Could not open the file!" << std::endl;
+        return 1;
+    }
+
+    while(file.good()){
+        getline(file, input,';');
+        std::cout << input << std::endl;
+    }
     // read files
-    readDocentesFile();
-    readVeiculosFile();
-    readPublicacoesFile();
-    readQualificacoesFile();
-    readRegrasPontuacaoFile();
+    // readDocentesFile();
+    // readVeiculosFile();
+    // readPublicacoesFile();
+    // readQualificacoesFile();
+    // readRegrasPontuacaoFile();
 
     // close files
-    fin.close();
+    file.close();
 
     return 0;
 }
