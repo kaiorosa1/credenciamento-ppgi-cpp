7d02aa5375e9906c3e1b61fb9d7650adfdafbdc1
Kaio Rosa <kaiosrosa@gmail.com>
2019-08-26 22:31:04 -0300

    change date type

--- docente.h
+++ docente.h
@@ -6,22 +6,22 @@
 class Docente {
     long codigo;
     std::string nome;
-    time_t data_nascimento;
-    time_t data_ingresso; // what is the best type to store date
+    tm data_nascimento;
+    tm data_ingresso; // what is the best type to store date
     bool is_coordenador;
     
     public:
     // getters and setters
     long get_codigo(){ return codigo;}
     std::string get_nome(){ return nome;}
-    time_t get_data_nascimento(){return data_nascimento;}
-    time_t get_data_ingresso(){return data_ingresso;}
+    tm get_data_nascimento(){return data_nascimento;}
+    tm get_data_ingresso(){return data_ingresso;}
     bool get_is_coodernador(){return is_coordenador;}
 
     void set_codigo(long cod){ codigo = cod;}
     void set_nome(std::string name){nome = name;}
-    void set_data_nascimento(time_t dt_nasc){data_ingresso = dt_nasc;}
-    void set_data_ingresso(time_t dt_ingresso){data_ingresso = dt_ingresso;}
+    void set_data_nascimento(tm dt_nasc){data_ingresso = dt_nasc;}
+    void set_data_ingresso(tm dt_ingresso){data_ingresso = dt_ingresso;}
     void set_is_coordenador(bool is_coord){is_coordenador = is_coord;}
 
 };
--- regras.h
+++ regras.h
@@ -7,8 +7,8 @@
 #include "qualis.h"
 
 class Regras {
-    time_t data_inicio;
-    time_t data_fim;
+    tm data_inicio;
+    tm data_fim;
     std::vector<Qualis> lista_qualis;
     //lista de pontos
     double multiplicador_periodicos;
@@ -17,16 +17,16 @@ class Regras {
 
     public:
     //getters and setters
-    time_t get_data_inicio(){return data_inicio;}
-    time_t get_data_fim(){return data_fim;}
+    tm get_data_inicio(){return data_inicio;}
+    tm get_data_fim(){return data_fim;}
     std::vector<Qualis> get_lista_qualis(){return lista_qualis;}
     //get lista pontos
     double get_multiplicador_periodicos(){return multiplicador_periodicos;}
     int get_qtd_anos_considerar(){return qtd_anos_considerar;}
     int get_pontuacao_minima(){return pontuacao_minima;}
 
-    void set_data_inicio(time_t dt_inicio){data_inicio=dt_inicio;}
-    void set_data_fim(time_t dt_fim){data_fim=dt_fim;}
+    void set_data_inicio(tm dt_inicio){data_inicio=dt_inicio;}
+    void set_data_fim(tm dt_fim){data_fim=dt_fim;}
     void set_lista_qualis(std::vector<Qualis> lq){lista_qualis=lq;}
     //set lista pontos
     void set_multiplicador_periodicos(double mult){multiplicador_periodicos=mult;}
